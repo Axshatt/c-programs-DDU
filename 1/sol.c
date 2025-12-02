87323792614b24bc7493f5b84aaffbb8487a66fc
Akshat Singh <codepreneur.in@gmail.com>
2025-12-02 11:48:08 +0530

    Solution 1

--- 1/sol.c
+++ 1/sol.c
@@ -1,6 +1,14 @@
-#include <stdio.h>
+// Program to find the area of a triangle using Heron's formula
 
-int main() {
-    printf("Hello, World!");
+#include <stdio.h>
+#include <math.h>
+int main(){
+    float a,b,c,s,area;
+    printf("Enter the lengths of the three sides of the triangle: ");
+    scanf("%f %f %f",&a,&b,&c);
+    s=(a+b+c)/2;
+    area=sqrt(s*(s-a)*(s-b)*(s-c));
+    printf("The area of the triangle is: %.2f\n",area);
     return 0;
+    
 }
